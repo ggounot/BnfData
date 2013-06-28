ea977a70c2b7e4e7749c91bb9c669f76e9a37b9f
Gérald Gounot <gerald@gounot.eu>
2013-06-28 16:20:35 +0200

    Add DB installation handling to the main activity

--- src/eu/gounot/bnfdata/MainActivity.java
+++ src/eu/gounot/bnfdata/MainActivity.java
@@ -1,11 +1,35 @@
 package eu.gounot.bnfdata;
 
+import android.animation.Animator;
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.ViewStub;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
-public class MainActivity extends BnfDataBaseActivity {
+import com.actionbarsherlock.view.Menu;
+
+import eu.gounot.bnfdata.database.DatabaseConstants;
+import eu.gounot.bnfdata.database.DatabaseInstallationListener;
+import eu.gounot.bnfdata.database.DatabaseInstallationTask;
+
+public class MainActivity extends BnfDataBaseActivity implements DatabaseInstallationListener {
 
     private static final String TAG = "MainActivity";
+
+    private DatabaseInstallationTask mDatabaseInstallationTask;
+
+    private boolean mDatabaseInstallationMode = false;
+
+    private View mHomeView;
+    private View mDbInstallationView;
+    private ProgressBar mProgressBar;
+    private TextView mPercentageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +44,172 @@ public class MainActivity extends BnfDataBaseActivity {
         getSupportActionBar().setHomeButtonEnabled(false);
 
         setContentView(R.layout.activity_main);
+
+        handleDatabaseInstallation();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+
+        // Disable search menu item when in database installation mode.
+        if (mDatabaseInstallationMode) {
+            getSearchMenuItem().setEnabled(false);
+        }
+
+        return true;
+    }
+
+    @Override
+    public Object onRetainCustomNonConfigurationInstance() {
+        if (BuildConfig.DEBUG) {
+            Log.d(TAG, "onRetainCustomNonConfigurationInstance()");
+        }
+
+        if (mDatabaseInstallationTask != null) {
+            mDatabaseInstallationTask.detach();
+        }
+
+        return mDatabaseInstallationTask;
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        if (BuildConfig.DEBUG) {
+            Log.d(TAG, "onDestroy()");
+        }
+
+        if (mDatabaseInstallationTask != null) {
+            mDatabaseInstallationTask.detach();
+        }
+    }
+
+    @Override
+    public boolean onSearchRequested() {
+        if (BuildConfig.DEBUG) {
+            Log.d(TAG, "onSearchRequested()");
+        }
+
+        // Disable search when in database install mode.
+        if (!mDatabaseInstallationMode) {
+            return super.onSearchRequested();
+        }
+
+        return false;
+    }
+
+    public void handleDatabaseInstallation() {
+        // Check the database setup status and install it if necessary.
+        mDatabaseInstallationTask = (DatabaseInstallationTask) getLastCustomNonConfigurationInstance();
+        if (mDatabaseInstallationTask != null) {
+            int progress = mDatabaseInstallationTask.getProgress();
+            if (progress < DatabaseConstants.PROGRESS_MAX) {
+                showDatabaseInstallationView();
+                onDatabaseInstallationProgressUpdate(progress);
+                mDatabaseInstallationTask.attach(this);
+            }
+        } else if (databaseInstallationIsNeeded()) {
+            showDatabaseInstallationView();
+            mDatabaseInstallationTask = new DatabaseInstallationTask(getApplicationContext(), this);
+            mDatabaseInstallationTask.execute();
+        }
+    }
+
+    public boolean databaseInstallationIsNeeded() {
+        if (BuildConfig.DEBUG) {
+            Log.d(TAG, "databaseInstallationIsNeeded()");
+        }
+
+        // Database installation is needed if it was not already fully installed
+        // or if the already installed version doesn't match the current one.
+        SharedPreferences preferences = getSharedPreferences(DatabaseConstants.PREFS_FILE_NAME,
+                Context.MODE_PRIVATE);
+        int dbState = preferences.getInt(DatabaseConstants.PREF_DB_STATE_KEY,
+                DatabaseConstants.DB_NOT_INSTALLED);
+        if (dbState == DatabaseConstants.DB_INSTALLED) {
+            int dbVersion = preferences.getInt(DatabaseConstants.PREF_DB_VERSION_KEY, 0);
+            return (dbVersion != DatabaseConstants.DB_VERSION);
+        } else {
+            return true;
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR1)
+    public void showDatabaseInstallationView() {
+        if (BuildConfig.DEBUG) {
+            Log.d(TAG, "showDatabaseInstallationView()");
+        }
+
+        // Enter database installation mode.
+        mDatabaseInstallationMode = true;
+
+        // Hide the home view.
+        mHomeView = findViewById(R.id.home_view);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR1) {
+            mHomeView.setAlpha(0);
+        } else {
+            mHomeView.setVisibility(View.GONE);
+        }
+
+        // Show the database installation view.
+        ViewStub dbInstallViewStub = (ViewStub) findViewById(R.id.database_installation_viewstub);
+        mDbInstallationView = dbInstallViewStub.inflate();
+
+        // Reference the progress bar for progress updates.
+        mProgressBar = (ProgressBar) mDbInstallationView
+                .findViewById(R.id.db_installation_progressbar);
+        mProgressBar.setMax(DatabaseConstants.PROGRESS_MAX);
+
+        // Reference the percentage view for progress updates.
+        mPercentageView = (TextView) mDbInstallationView
+                .findViewById(R.id.db_installation_percentage);
+    }
+
+    @Override
+    public void onDatabaseInstallationProgressUpdate(int progress) {
+        // Update the progress bar and percentage view.
+        mProgressBar.setProgress(progress);
+        mPercentageView.setText(Integer.toString(progress));
+    }
+
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR1)
+    @Override
+    public void onDatabaseInstallationComplete() {
+        // No longer in database installation mode. Enable the search menu item.
+        mDatabaseInstallationMode = false;
+        getSearchMenuItem().setEnabled(true);
+
+        // Hide the database installation view and show the home view.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR1) {
+            mHomeView.animate().alpha(1);
+            mDbInstallationView.animate().alpha(0).setListener(new Animator.AnimatorListener() {
+
+                @Override
+                public void onAnimationStart(Animator animation) {
+                    // Nothing to do.
+                }
+
+                @Override
+                public void onAnimationRepeat(Animator animation) {
+                    // Nothing to do.
+                }
+
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mDbInstallationView.setVisibility(View.GONE);
+                }
+
+                @Override
+                public void onAnimationCancel(Animator animation) {
+                    // Nothing to do.
+                }
+            });
+        } else {
+            mDbInstallationView.setVisibility(View.GONE);
+            mHomeView.setVisibility(View.VISIBLE);
+        }
     }
 
 }
