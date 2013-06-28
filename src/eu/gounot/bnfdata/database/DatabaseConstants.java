75f3dce8ab0be60b66875ae68f1971f5dc835a11
Gérald Gounot <gerald@gounot.eu>
2013-06-28 15:56:52 +0200

    Add various constants related to the database
    They are mainly useful for the database installation process.

---
+++ src/eu/gounot/bnfdata/database/DatabaseConstants.java
@@ -0,0 +1,19 @@
+package eu.gounot.bnfdata.database;
+
+public class DatabaseConstants {
+
+    public static final String ASSETS_COMPRESSED_DB_FILENAME = "suggestions.jpg";
+    public static final String DB_FILENAME = "suggestions.db";
+
+    public static final int UNCOMPRESSED_DB_SIZE = 26257408;
+    public static final int PROGRESS_MAX = 100;
+    public static final int DB_VERSION = 1;
+
+    public static final String PREFS_FILE_NAME = "prefs";
+    public static final String PREF_DB_STATE_KEY = "dbstate";
+    public static final String PREF_DB_VERSION_KEY = "dbversion";
+
+    public static final int DB_NOT_INSTALLED = 0;
+    public static final int DB_INSTALL_ABORTED = 1;
+    public static final int DB_INSTALLED = 2;
+}
