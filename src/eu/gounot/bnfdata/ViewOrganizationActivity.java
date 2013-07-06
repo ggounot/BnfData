f65b951e6965e7e850eb17341bd29c72dc1e08ac
Gérald Gounot <gerald@gounot.eu>
2013-07-06 22:29:30 +0200

    Make ViewOrganizationActivity extend ViewObjectActivity

--- src/eu/gounot/bnfdata/ViewOrganizationActivity.java
+++ src/eu/gounot/bnfdata/ViewOrganizationActivity.java
@@ -2,56 +2,30 @@ package eu.gounot.bnfdata;
 
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.TextUtils;
+import android.text.method.LinkMovementMethod;
 import android.util.Log;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.ImageView;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 
-import eu.gounot.bnfdata.loadercallbacks.OrganizationDataLoaderCallbacks;
+import eu.gounot.bnfdata.data.DataObject;
+import eu.gounot.bnfdata.data.Organization;
+import eu.gounot.bnfdata.loadercallbacks.OrganizationImageLoaderCallbacks;
 import eu.gounot.bnfdata.util.Constants;
 
-public class ViewOrganizationActivity extends BnfDataBaseActivity implements OnClickListener {
+public class ViewOrganizationActivity extends ViewObjectActivity {
 
     private static final String TAG = "ViewOrganizationActivity";
 
-    // Loaders' IDs.
-    public static final int DATA_LOADER = 0;
-    public static final int IMAGE_LOADER = 1;
-
     // Views.
-    private ImageView mImageView;
-    private TextView mNameTextView;
-    private TextView mStartdateLabelTextView;
-    private TextView mStopdateLabelTextView;
-    private TextView mNationalitiesLabelTextView;
-    private TextView mNotesLabelTextView;
-    private TextView mStartdateTextView;
-    private TextView mStopdateTextView;
-    private TextView mNationalitiesTextView;
-    private TextView mNotesTextView;
-    private ProgressBar mProgressBar;
-    private ProgressBar mImageProgressBar;
-    private View mNetworkErrorView;
+    private View mProgressBar;
     private View mScrollView;
 
-    private OrganizationDataLoaderCallbacks mDataLoaderCallbacks;
-
-    public ProgressBar getProgressBar() {
-        return mProgressBar;
-    }
-
-    public ProgressBar getImageProgressBar() {
-        return mImageProgressBar;
-    }
-
-    public View getScrollView() {
-        return mScrollView;
-    }
-
-    public void setNetworkErrorView(View networkErrorView) {
-        mNetworkErrorView = networkErrorView;
+    @Override
+    public int getObjectType() {
+        return Constants.OBJECT_TYPE_ORGANIZATION;
     }
 
     @Override
@@ -64,91 +38,202 @@ public class ViewOrganizationActivity extends BnfDataBaseActivity implements OnC
 
         setContentView(R.layout.activity_view_organization);
 
-        // Reference the views.
-        mImageView = (ImageView) findViewById(R.id.image);
-        mNameTextView = (TextView) findViewById(R.id.name);
-        mStartdateLabelTextView = (TextView) findViewById(R.id.startdate_label);
-        mStopdateLabelTextView = (TextView) findViewById(R.id.stopdate_label);
-        mNationalitiesLabelTextView = (TextView) findViewById(R.id.country_label);
-        mNotesLabelTextView = (TextView) findViewById(R.id.notes_label);
-        mStartdateTextView = (TextView) findViewById(R.id.startdate);
-        mStopdateTextView = (TextView) findViewById(R.id.stopdate);
-        mNationalitiesTextView = (TextView) findViewById(R.id.country);
-        mNotesTextView = (TextView) findViewById(R.id.notes);
-        mProgressBar = (ProgressBar) findViewById(R.id.progress_bar);
-        mImageProgressBar = (ProgressBar) findViewById(R.id.image_progress_bar);
+        mProgressBar = findViewById(R.id.progress_bar);
         mScrollView = findViewById(R.id.scrollview);
 
-        // Get the ARK name from the intent.
+        // Get the ARK name from the intent and load the data.
         String arkName = getIntent().getExtras().getString(Constants.INTENT_ARK_NAME_KEY);
-
-        // Initialize the data loader with its callbacks.
-        mDataLoaderCallbacks = new OrganizationDataLoaderCallbacks(this, arkName);
-        getSupportLoaderManager().initLoader(DATA_LOADER, null, mDataLoaderCallbacks);
+        loadData(arkName);
     }
 
     @Override
-    public void onClick(View v) {
-        // Following a network error, the user has clicked the Retry button,
-        // so we restart the loader to try loading the data again.
-        mNetworkErrorView.setVisibility(View.INVISIBLE);
-        mProgressBar.setVisibility(View.VISIBLE);
-        getSupportLoaderManager().restartLoader(DATA_LOADER, null, mDataLoaderCallbacks);
+    public void onDataLoaded(DataObject dataObject) {
+        if (BuildConfig.DEBUG) {
+            Log.d(TAG, "onDataLoaded()");
+        }
+
+        Organization organization = (Organization) dataObject;
+
+        setName(organization.getName());
+        setCountry(organization.getCountry());
+        setLanguage(organization.getLanguageName());
+        setCreationDate(organization.getCreationDate());
+        setTerminationDate(organization.getTerminationDate());
+        setHomePage(organization.getHomePage());
+        setHistory(organization.getHistory());
+        setFieldsOfActivity(organization.getFieldsOfActivity());
+        setAltForms(organization.getAltForms());
+        setEditorialNotes(organization.getEditorialNotes());
+        setExternalLinks(organization.getCatalogueUrl(), organization.getWikipediaUrl());
+
+        String imageUrl = organization.getImageUrl();
+        if (imageUrl != null) {
+            OrganizationImageLoaderCallbacks callbacks = new OrganizationImageLoaderCallbacks(this,
+                    imageUrl);
+            getSupportLoaderManager().initLoader(IMAGE_LOADER, null, callbacks);
+        } else {
+            setImage(null);
+        }
+
+        hideNetworkError();
+
+        // Hide the progress bar.
+        mProgressBar.setVisibility(View.GONE);
+
+        // Show the ScrollView.
+        mScrollView.setVisibility(View.VISIBLE);
     }
 
-    // These methods below are used by the onLoadFinished() callback
-    // to update the UI with the loaded data.
+    @Override
+    public void onImageLoaded(Bitmap bitmap) {
+        setImage(bitmap);
+    }
+
+    // These methods below are used to update the UI with the loaded data.
 
     public void setImage(Bitmap image) {
+        ImageView imageView = (ImageView) findViewById(R.id.image);
         if (image != null) {
-            mImageView.setImageBitmap(image);
+            imageView.setImageBitmap(image);
         } else {
-            mImageView.setImageResource(R.drawable.no_author_or_organization_image);
+            imageView.setImageResource(R.drawable.no_work_image);
         }
-        mImageProgressBar.setVisibility(View.GONE);
+        findViewById(R.id.image_progress_bar).setVisibility(View.GONE);
     }
 
     public void setName(String name) {
+        TextView nameTextView = (TextView) findViewById(R.id.name);
         if (name != null) {
-            mNameTextView.setText(name);
+            nameTextView.setText(name);
         } else {
-            mNameTextView.setVisibility(View.GONE);
+            nameTextView.setVisibility(View.GONE);
         }
     }
 
-    public void setStartdate(String startdate) {
-        if (startdate != null) {
-            mStartdateTextView.setText(startdate);
+    public void setCountry(String country) {
+        TextView countryLabelTextView = (TextView) findViewById(R.id.country_label);
+        TextView countryTextView = (TextView) findViewById(R.id.country);
+        if (country != null) {
+            countryTextView.setText(country);
         } else {
-            mStartdateLabelTextView.setVisibility(View.GONE);
-            mStartdateTextView.setVisibility(View.GONE);
+            countryLabelTextView.setVisibility(View.GONE);
+            countryTextView.setVisibility(View.GONE);
         }
     }
 
-    public void setStopdate(String stopdate) {
-        if (stopdate != null) {
-            mStopdateTextView.setText(stopdate);
+    public void setLanguage(String language) {
+        TextView languageLabelTextView = (TextView) findViewById(R.id.language_label);
+        TextView languageTextView = (TextView) findViewById(R.id.language);
+        if (language != null) {
+            languageTextView.setText(language);
         } else {
-            mStopdateLabelTextView.setVisibility(View.GONE);
-            mStopdateTextView.setVisibility(View.GONE);
+            languageLabelTextView.setVisibility(View.GONE);
+            languageTextView.setVisibility(View.GONE);
         }
     }
 
-    public void setNationalities(String nationalities) {
-        if (nationalities != null) {
-            mNationalitiesTextView.setText(nationalities);
+    public void setCreationDate(String creationDate) {
+        TextView creationDateLabelTextView = (TextView) findViewById(R.id.creation_date_label);
+        TextView creationDateTextView = (TextView) findViewById(R.id.creation_date);
+        if (creationDate != null) {
+            creationDateTextView.setText(creationDate);
         } else {
-            mNationalitiesLabelTextView.setVisibility(View.GONE);
-            mNationalitiesTextView.setVisibility(View.GONE);
+            creationDateLabelTextView.setVisibility(View.GONE);
+            creationDateTextView.setVisibility(View.GONE);
         }
     }
 
-    public void setNotes(String notes) {
-        if (notes != null) {
-            mNotesTextView.setText(notes);
+    public void setTerminationDate(String terminationDate) {
+        TextView terminationDateLabelTextView = (TextView) findViewById(R.id.termination_date_label);
+        TextView terminationDateTextView = (TextView) findViewById(R.id.termination_date);
+        if (terminationDate != null) {
+            terminationDateTextView.setText(terminationDate);
         } else {
-            mNotesLabelTextView.setVisibility(View.GONE);
-            mNotesTextView.setVisibility(View.GONE);
+            terminationDateLabelTextView.setVisibility(View.GONE);
+            terminationDateTextView.setVisibility(View.GONE);
+        }
+    }
+
+    public void setHomePage(String homePage) {
+        TextView homePageDateLabelTextView = (TextView) findViewById(R.id.home_page_label);
+        TextView homePageDateTextView = (TextView) findViewById(R.id.home_page);
+        if (homePage != null) {
+            homePageDateTextView.setText(homePage);
+        } else {
+            homePageDateLabelTextView.setVisibility(View.GONE);
+            homePageDateTextView.setVisibility(View.GONE);
+        }
+    }
+
+    public void setHistory(String[] history) {
+        TextView historyLabelTextView = (TextView) findViewById(R.id.history_label);
+        TextView historyTextView = (TextView) findViewById(R.id.history);
+        if (history != null) {
+            historyTextView.setText(TextUtils.join("\n", history));
+        } else {
+            historyLabelTextView.setVisibility(View.GONE);
+            historyTextView.setVisibility(View.GONE);
+        }
+    }
+
+    public void setFieldsOfActivity(String[] fieldsOfActivity) {
+        TextView fieldsOfActivityLabelTextView = (TextView) findViewById(R.id.fields_of_activity_label);
+        TextView fieldsOfActivityTextView = (TextView) findViewById(R.id.fields_of_activity);
+        if (fieldsOfActivity != null) {
+            fieldsOfActivityTextView.setText(TextUtils.join("\n", fieldsOfActivity));
+        } else {
+            fieldsOfActivityLabelTextView.setVisibility(View.GONE);
+            fieldsOfActivityTextView.setVisibility(View.GONE);
+        }
+    }
+
+    public void setAltForms(String[] altForms) {
+        TextView altFormsLabelTextView = (TextView) findViewById(R.id.alt_forms_label);
+        TextView altFormsTextView = (TextView) findViewById(R.id.alt_forms);
+        if (altForms != null) {
+            altFormsTextView.setText(TextUtils.join("\n", altForms));
+        } else {
+            altFormsLabelTextView.setVisibility(View.GONE);
+            altFormsTextView.setVisibility(View.GONE);
+        }
+    }
+
+    public void setEditorialNotes(String[][] editorialNotes) {
+        TextView editorialNotesLabelTextView = (TextView) findViewById(R.id.editorial_notes_label);
+        TextView editorialNotesTextView = (TextView) findViewById(R.id.editorial_notes);
+        if (editorialNotes != null) {
+            String[] noteGroups = new String[editorialNotes.length];
+            for (int i = 0; i < editorialNotes.length; i++) {
+                noteGroups[i] = TextUtils.join("\n", editorialNotes[i]);
+            }
+            editorialNotesTextView.setText(TextUtils.join("\n\n", noteGroups));
+        } else {
+            editorialNotesLabelTextView.setVisibility(View.GONE);
+            editorialNotesTextView.setVisibility(View.GONE);
+        }
+    }
+
+    public void setExternalLinks(String catalogueUrl, String wikipediaUrl) {
+        TextView externalLinksLabelTextView = (TextView) findViewById(R.id.external_links_label);
+        TextView externalLinksTextView = (TextView) findViewById(R.id.external_links);
+        String urls = "";
+        if (catalogueUrl != null) {
+            urls += "<a href=\"" + catalogueUrl + "\">"
+                    + getResources().getString(R.string.catalogue_link_text) + "</a>";
+        }
+        if (wikipediaUrl != null) {
+            if (!urls.equals("")) {
+                urls += "<br />";
+            }
+            urls += "<a href=\"" + wikipediaUrl + "\">"
+                    + getResources().getString(R.string.wikipedia_link_text) + "</a>";
+        }
+        if (!urls.equals("")) {
+            externalLinksTextView.setText(Html.fromHtml(urls));
+            externalLinksTextView.setMovementMethod(LinkMovementMethod.getInstance());
+        } else {
+            externalLinksLabelTextView.setVisibility(View.GONE);
+            externalLinksTextView.setVisibility(View.GONE);
         }
     }
 
