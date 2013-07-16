e43e0b541fec3969a4f7b78d08504227acf81e88
Gérald Gounot <gerald@gounot.eu>
2013-07-16 22:41:42 +0200

    DataServer: use HttpURLConnection
    It is recommended to use HttpURLConnection instead of DefaultHttpClient.
    
    See:
    - http://developer.android.com/reference/org/apache/http/impl/client/DefaultHttpClient.html
    - http://android-developers.blogspot.fr/2011/09/androids-http-clients.html

--- src/eu/gounot/bnfdata/util/DataServer.java
+++ src/eu/gounot/bnfdata/util/DataServer.java
@@ -4,15 +4,11 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 import android.util.Log;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.params.HttpProtocolParams;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -21,11 +17,6 @@ import eu.gounot.bnfdata.BuildConfig;
 public class DataServer {
 
     private static final String TAG = "DataServer";
-
-    private static HttpClient mHttpClient = new DefaultHttpClient();
-    static {
-        HttpProtocolParams.setUserAgent(mHttpClient.getParams(), Constants.USER_AGENT);
-    }
 
     public static JSONObject getJsonObject(int objectType, String arkName) throws IOException,
             JSONException {
@@ -52,10 +43,9 @@ public class DataServer {
             Log.d(TAG, "downloadJsonText() url=" + url);
         }
 
-        HttpGet httpGet = new HttpGet(url);
-        HttpResponse response = mHttpClient.execute(httpGet);
-        HttpEntity entity = response.getEntity();
-        InputStream inputStream = entity.getContent();
+        HttpURLConnection urlConnection = (HttpURLConnection) new URL(url).openConnection();
+        urlConnection.setRequestProperty("User-Agent", Constants.USER_AGENT);
+        InputStream inputStream = urlConnection.getInputStream();
         BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
         StringBuilder stringBuilder = new StringBuilder();
         String line;
