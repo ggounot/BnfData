5632d65b9a141ba1fbe850d7006cee72903303ac
Gérald Gounot <gerald@gounot.eu>
2013-07-21 00:07:47 +0200

    AboutActivity: add section to see the changelog

--- src/eu/gounot/bnfdata/AboutActivity.java
+++ src/eu/gounot/bnfdata/AboutActivity.java
@@ -1,11 +1,18 @@
 package eu.gounot.bnfdata;
 
 import android.os.Bundle;
+import android.text.Spannable;
+import android.text.TextPaint;
 import android.text.method.LinkMovementMethod;
+import android.text.style.CharacterStyle;
 import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.TextView;
 
 import com.actionbarsherlock.view.Menu;
+
+import eu.gounot.bnfdata.dialog.ChangelogDialogFragment;
 
 public class AboutActivity extends BnfDataBaseActivity {
 
@@ -25,6 +32,8 @@ public class AboutActivity extends BnfDataBaseActivity {
         generalites2CodeTextView.setMovementMethod(LinkMovementMethod.getInstance());
         TextView sourceCodeTextView = (TextView) findViewById(R.id.source_code);
         sourceCodeTextView.setMovementMethod(LinkMovementMethod.getInstance());
+
+        makeLinkToChangelogDialog();
     }
 
     @Override
@@ -38,6 +47,39 @@ public class AboutActivity extends BnfDataBaseActivity {
         configureSearchView(this, menu);
 
         return true;
+    }
+
+    private void makeLinkToChangelogDialog() {
+        TextView seeChangelogTextView = (TextView) findViewById(R.id.changelog);
+        seeChangelogTextView.setMovementMethod(LinkMovementMethod.getInstance());
+        Spannable seeChangelogSpannable = (Spannable) seeChangelogTextView.getText();
+
+        // Here we should normally use a ClickableSpan but it causes ChangelogDialogFragment.show()
+        // to raise an exception when it is called a second time after a screen rotation. This issue
+        // doesn't happen with an OnClickListener so we use this instead and give a hyperlink style
+        // to the text to mimic a real hyperlink.
+
+        // Give a hyperlink style to the text.
+        CharacterStyle style = new CharacterStyle() {
+
+            @Override
+            public void updateDrawState(TextPaint tp) {
+                tp.setColor(tp.linkColor);
+                tp.setUnderlineText(true);
+            }
+        };
+        seeChangelogSpannable.setSpan(style, 0, seeChangelogSpannable.length(),
+                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        // Attach an OnClickListener that shows the ChangelogDialogFragment when the TextView is
+        // clicked.
+        seeChangelogTextView.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                new ChangelogDialogFragment().show(getSupportFragmentManager(), "changelog");
+            }
+        });
     }
 
 }
